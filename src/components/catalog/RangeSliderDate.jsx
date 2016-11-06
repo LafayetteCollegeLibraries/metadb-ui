3fedf68cbeb96786c1a83e4857adbfc45ec06bd0
Adam Malantonio <malantoa@lafayette.edu>
2016-11-06 10:23:38 -0500

    clear up some file names

--- src/components/catalog/RangeSliderDate.jsx
+++ src/components/catalog/RangeSliderDate.jsx
@@ -1,207 +1,267 @@
 import React from 'react'
-import Button from '../Button.jsx'
 import Slider from 'rc-slider'
+import Button from '../Button.jsx'
 
-import formatDateValue from './common/format-date-value'
-import roundDateValue from './common/round-date-to-interval'
-import parseInputValue from './common/parse-input-date-value'
-
-import {
-	INTERVALS,
-	VALUES as INTERVAL_VALUES,
-} from './common/date-intervals'
-
-// TODO: move this styling to SCSS area
 import 'rc-slider/assets/index.css'
 
-const { YEAR, MONTH, DAY } = INTERVALS
+const T = React.PropTypes
 
-const propTypes = {
-	// min/max are UTC timestamp values (similar to Date.now())
-	min: React.PropTypes.number.isRequired,
-	max: React.PropTypes.number.isRequired,
-
-	onApplyRange: React.PropTypes.func.isRequired,
-
-	interval: React.PropTypes.oneOf(INTERVAL_VALUES),
+const INTERVALS = {
+	DAY: 'day',
+	MONTH: 'month',
+	YEAR: 'year',
 }
 
-const defaultProps = {
-	interval: DAY,
-}
+const RangeSliderDate = React.createClass({
+	propTypes: {
+		interval: T.oneOf([INTERVALS.DAY, INTERVALS.MONTH, INTERVALS.YEAR]),
 
-// helper fns
-function getInputType (interval) {
-	switch (interval) {
-		case MONTH: return 'month'
-		case YEAR:  return 'number'
-		case DAY:
-		default:    return 'date'
-	}
-}
+		// expect min/max/value to be Date.UTC values
+		// (handled at the level of the wrapper)
+		max: T.number.isRequired,
+		min: T.number.isRequired,
+		
+		onApplyRange: T.func.isRequired,
 
-function getStepValue (min, max, interval) {
-	const day = 1000 * 60 * 60 * 24
 
-	let divisor
+	},
 
-	switch (interval) {
-		case YEAR:  divisor = day * 365.25
-		case MONTH: divisor = (day * 365.25) / 12
-		case DAY:
-		default:    divisor = day
-	}
+	getDefaultProps: function () {
+		return { 
+			interval: INTERVALS.DAY,
+		}
+	},
 
-	return Math.round((max - min) / divisor)
-}
+	getInitialState: function () {
+		return {
+			value: [
+				this.props.min,
+				this.props.max,
+			]
+		}
+	},
 
-class RangeSliderDate extends React.Component {
-	constructor (props) {
-		super(props)
+	// determine which `type=` value to use for the input
+	getDateInputType: function () {
+		switch (this.props.interval) {
+			case INTERVALS.MONTH:
+				return 'month'
 
-		this.state = {
-			min: props.min,
-			max: props.max,
+			case INTERVALS.DAY:
+				return 'date'
+
+			case INTERVALS.YEAR:
+			default:
+				return 'number'
+		}
+	},
+
+	// sets a timestamp to YYYY(-MM(-DD))
+	// dependent on `props.interval`
+	getFormattedDateValue: function (raw) {
+		const d8 = new Date(raw)
+		const formatDay = (d) => {
+			const yr = d.getUTCFullYear()
+			const day = d.getUTCDate()
+			let mo = d.getUTCMonth() + 1
+
+			if (mo < 10)
+				mo = '0' + mo
+
+			return `${yr}-{$mo}-${day}`
 		}
 
-		this._formatted = {
-			min: formatDateValue(props.interval, props.min),
-			max: formatDateValue(props.interval, props.max)
+		const formatMonth = (d) => {
+			const yr = d.getUTCFullYear()
+			let mo = d.getUTCMonth() + 1
+
+			if (mo < 10)
+				mo = '0' + mo
+
+			return `${yr}-${mo}`
 		}
 
-		this._rounded = {
-			min: roundDateValue(props.interval, props.min),
-			max: roundDateValue(props.interval, props.max),
+		const formatYear = (d) => {
+			return `${d.getUTCFullYear()}`
 		}
 
-		this.getStepValue = this.getStepValue.bind(this)
-		this.handleApplyRange = this.handleApplyRange.bind(this)
-		// this.handleInputChange is bound within this.renderInput
-		this.renderInput = this.renderInput.bind(this)
-		this.renderSlider = this.renderSlider.bind(this)
-	}
+		switch (this.props.interval) {
+			case INTERVALS.MONTH:
+				return formatMonth(d8)
 
-	componentWillUpdate (nextProps) {
-		if (nextProps.interval !== this.props.interval)
-			this.step = null
-	}
+			case INTERVALS.DAY:
+				return formatDay(d8)
 
-	getStepValue () {
-		if (!this._step && this._step !== 0) {
-			const { min, max } = this._rounded ? this._rounded : this.props
-			const { interval } = this.props
-			this._step = getStepValue(min, max, interval)
+			case INTERVALS.YEAR:
+			default:
+				return formatYear(d8)
+		}
+	},
+
+	// rc-slider will complain if max-min isn't evenly divisible by
+	// the step value (not something I expected would happen, but 
+	// it _is_). so we'll do this math to get a clean step + do the
+	// rounding when we send the value with `onApplyRange`
+	getStepValue: function () {
+		const { min, max } = this.props
+		let divisor
+
+		switch (this.props.interval) {
+			case INTERVALS.DAY:
+				divisor = 864e+5 // 1000ms * 60s * 60m * 24h
+
+			case INTERVALS.MONTH:
+				divisor = 26298e+5 // (DAY * 365.25) / 12
+
+			case INTERVALS.YEAR:
+			default:
+				divisor = 315576e+5 // DAY * 365.25
 		}
 
-		return this._step
-	}
+		return Math.round((max - min) / divisor)
+	},
 
-	handleApplyRange () {
-		const value = [
-			this.state.min,
-			this.state.max,
-		].map(v => formatDateValue(this.props.interval, v))
+	// round range values to their nearest `interval` 
+	// (months are set to their first day; days are set to midnight)
+	handleApplyRange: function () {
+		if (!this.props.onApplyRange)
+			return
 
-		this.props.onApplyRange(value)
-	}
+		const cleaned = this.state.value.map(this.roundDateValue)
 
-	handleInputChange (which, event) {
-		const target = (event || {}).target || {}
-		const value = target.value
-		const parsed = parseInputValue(value)
+		this.props.onApplyRange.call(null, cleaned)
+	},
 
-		const update = {}
-		// (parsed !== parsed) === Number.isNaN(parsed)
-		update[which] = parsed !== parsed ? null : parsed
+	handleMaxValueChange: function (ev) {
+		const val = ev.target.value
+		const args = val.split('-').filter(Boolean).map(Number)
+		const max = Date.UTC.apply(Date, args)
 
-		this.setState(update)
-	}
+		this.setState({
+			value: [
+				this.state.value[0],
+				max,
+			]
+		})
+	},
 
-	renderInput (which) {
-		const tsValue = this.state[which]
-		const value = tsValue ? formatDateValue(this.props.interval, tsValue) : ''
-		const type = getInputType(this.props.interval)
+	handleMinValueChange: function (ev) {
+		const val = ev.target.value
+		const args = val.split('-').filter(Boolean).map(Number)
+		const min = Date.UTC.apply(Date, args)
 
-		const props = {
-			className: 'range-slider-date--input',
-			key: `input-${which}`,
-			min: this._formatted.min,
-			max: this._formatted.max,
-			onChange: this.handleInputChange.bind(this, which),
-			type,
-			value,
+		this.setState({
+			value: [
+				min,
+				this.state.value[1],
+			]
+		})
+	},
+
+	handleSliderChange: function (value) {
+		this.setState({value})
+	},
+
+	renderInputPair: function (which, onChange, value) {
+		const id = 'input-' + which
+
+		const containerProps = {
+			style: {
+				display: 'inline-block',
+			},
+		}
+
+		const labelProps = {
+			children: which,
+			htmlFor: id,
+			key: which + '-label',
+		}
+
+		const inputProps = {
+			id,
+			key: which + '-input',
+			onChange,
+			type: this.getDateInputType(),
+			max: this.getFormattedDateValue(this.props.max),
+			min: this.getFormattedDateValue(this.props.min),
+			value: this.getFormattedDateValue(value),
+			style: {
+				display: 'block',
+				fontFamily: 'sans-serif',
+				fontSize: '.9em',
+				marginBottom: '10px',
+				marginRight: '10px',
+				minWidth: '5em',
+			}
 		}
 
 		return (
-			<label className="range-slider-date--label">
-				{which}
-				<input {...props} />
-			</label>
+			<div {...containerProps}>
+				<label {...labelProps} />
+				<input {...inputProps} />
+			</div>
 		)
-	}
+	},
 
-	renderSlider () {
-		const hasSingleValue = this.props.min === this.props.max
+	renderInputs: function () {
+		const [min, max] = this.state.value
 
-		const value = hasSingleValue ? (this.state.min || 0) : [
-			this.state.min || this.props.min,
-			this.state.max || this.props.max,
-		]
-
-		const onChange = value => {
-			this.setState({
-				min: value[0],
-				max: value[1],
-			})
+		const wrapperProps = {
+			style: {
+				marginBottom: '10px',
+			}
 		}
 
+		return (
+			<div {...wrapperProps}>
+				{this.renderInputPair('min', this.handleMinValueChange, min)}
+				{this.renderInputPair('max', this.handleMaxValueChange, max)}
+				<Button onClick={this.handleApplyRange}>Apply range</Button>
+			</div>
+		)
+	},
+
+	renderSlider: function () {
 		const props = {
 			allowCross: true,
-			min: this._rounded.min,
-			max: this._rounded.max,
-			onChange,
+			min: this.props.min,
+			max: this.props.max,
+			onChange: this.handleSliderChange,
 			pushable: false,
-			range: !hasSingleValue,
-			tipFormatter: formatDateValue.bind(null, this.props.interval),
-			value,
+			range: true,
+			step: this.getStepValue(),
+			tipFormatter: this.getFormattedDateValue,
+			value: this.state.value,
 		}
 
-		if (!hasSingleValue) {
-			props.step = this.getStepValue()
+		return React.createElement(Slider, props)
+	},
+
+	roundDateValue: function (val) {
+		const d = new Date(val)
+		const year = d.getUTCFullYear()
+
+		switch (this.props.interval) {
+			case INTERVALS.DAY:
+				return Date.UTC(year, d.getUTCMonth(), d.getUTCDate())
+				
+			case INTERVALS.MONTH:
+				return Date.UTC(year, d.getUTCMonth(), 1)
+
+			case INTERVALS.YEAR:
+			default:
+				return Date.UTC(year, 0, 1)
 		}
+	},
 
-		return <Slider {...props} />
-	}
-
-	render () {
-		const hasOneValue = this.props.min === this.props.max
-		const applyRangeProps = {
-			onClick: this.handleApplyRange
-		}
-
-		// disable the ability to set a range when either
-		// the min or max are empty
-		if (!this.state.min || !this.state.max) {
-			applyRangeProps.disabled = true
-		}
-
+	render: function () {
 		return (
-			<div className="range-slider-date">
-				<div className="range-slider-date--inputs-container">
-					{this.renderInput('min')}
-					{this.renderInput('max')}
-					<Button {...applyRangeProps}>
-						Apply range
-					</Button>
-				</div>
-
+			<div>
+				{this.renderInputs()}
 				{this.renderSlider()}
 			</div>
 		)
 	}
-}
-
-RangeSliderDate.propTypes = propTypes
-RangeSliderDate.defaultProps = defaultProps
+})
 
 export default RangeSliderDate
