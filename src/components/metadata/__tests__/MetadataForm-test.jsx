b4c8c731ec757b5f25bf9652e9f35035755ef141
Adam Malantonio <malantoa@lafayette.edu>
2016-11-28 15:59:22 -0500

    fix failing tests; position work-edit buttons

--- src/components/metadata/__tests__/MetadataForm-test.jsx
+++ src/components/metadata/__tests__/MetadataForm-test.jsx
@@ -45,20 +45,21 @@ describe('<MetadataForm />', function () {
 		it('passes values from `defaultProps` to children missing that prop', function () {
 			const PROP_KEY = 'someRandomProp'
 			const optsWithProp = {[PROP_KEY]: 'value'}
+			const renderer = props => <p>{props.value}</p>
 
 			const children = [
-				<FormField name="with-prop" {...optsWithProp} />,
-				<FormField name="without-prop" />,
+				<FormField name="with-prop" {...optsWithProp} renderer={renderer}/>,
+				<FormField name="without-prop" renderer={renderer} />,
 			]
 
 			const defaultProps = {
 				[PROP_KEY]: 'someRandomValue',
 			}
 
-			const $el = shallowEl({children, defaultProps})
+			const $el = mountEl({defaultProps, children})
 
-			const $withProp = $el.children().first()
-			const $withoutProp = $el.children().last()
+			const $withProp = $el.children().findWhere($c => $c.prop('name') === 'with-prop')
+			const $withoutProp = $el.children().findWhere($c => $c.prop('name') === 'without-prop')
 
 			expect($withProp.prop(PROP_KEY)).to.not.equal(defaultProps[PROP_KEY])
 			expect($withoutProp.prop(PROP_KEY)).to.equal(defaultProps[PROP_KEY])
