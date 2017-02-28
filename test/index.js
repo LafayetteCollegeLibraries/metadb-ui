ba4d15bd8bd29fbd7ec32798f8c98ad81b300c17
Adam Malantonio <malantoa@lafayette.edu>
2017-02-28 14:35:36 -0500

    normalize test names

--- test/index.js
+++ test/index.js
@@ -8,12 +8,12 @@ const mockServer = spawn('npm', ['run', 'mock-server'], {stdio: 'inherit'})
 
 // boots the simple static server that allows us to access files
 // in the `build` directory
-const testApp = spawn('npm', ['run', 'test-app'], {stdio: 'inherit'})
+const testApp = spawn('npm', ['run', 'test:app'], {stdio: 'inherit'})
 
 // a) bundles the app to `build/bundle.js` and `build/style.css`
 // b) runs the nightwatch tests
 // c) removes the aforementioned build files
-const e2e = spawn('npm', ['run', 'test-e2e-loop'], {stdio: 'inherit'})
+const e2e = spawn('npm', ['run', 'e2e-loop'], {stdio: 'inherit'})
 
 // shut down the mock server + test app server after the e2e loop runs
 e2e.on('close', () => {
