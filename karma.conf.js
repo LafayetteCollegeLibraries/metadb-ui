93162e5e84536cfacee9aa854e2e4a55397426d4
adam malantonio <malantoa@lafayette.edu>
2017-04-04 11:13:28 -0400

    add coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function(config) {
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['mocha', 'coverage'],
+		reporters: ['mocha', 'coverage', 'coveralls'],
 
 		// web server port
 		port: 9876,
@@ -107,9 +107,8 @@ module.exports = function(config) {
 		concurrency: Infinity,
 
 		coverageReporter: {
-			type: 'html',
+			type: 'lcov',
 			dir: 'coverage/',
-			file: 'coverage.html'
 		}
 	})
 }
